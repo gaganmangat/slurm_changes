eb2541c1d54e191e29021255f946d76dc0b8f849
gaganmangat <gaganm06@gmail.com>
2022-04-28 23:50:00 +0530

    changed path

--- src/slurmctld/calc_hops.c
+++ src/slurmctld/calc_hops.c
@@ -607,7 +607,7 @@ float ring(int arr[], int cnt){
 void hop(struct job_record *job_ptr)
 {
 	FILE *f;
-	f = fopen ("/home/ubuntu/workload/hops.txt", "a");
+	f = fopen ("/home/gagandeep/slurmcost/hops.txt", "a");
 	int i,j, begin, end,k=0;
 	int size = job_ptr->node_cnt;
 	int switches[size];
@@ -707,7 +707,7 @@ void hop(struct job_record *job_ptr)
 
 	// Add this information to a file
 	FILE *info;
-	info = fopen("/home/ubuntu/workload/debug.txt","a");
+	info = fopen("/home/gagandeep/slurmcost/debug.txt","a");
 	fputs(job_info,info); // Append jobinfo 
         fprintf(info,"\n");
 	
